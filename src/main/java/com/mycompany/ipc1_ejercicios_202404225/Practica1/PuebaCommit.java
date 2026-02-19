bb73676f7f50c921b7b86aa2f0bca19b38f8a63b
Rodrigo Gonzalez <3303393621202@ingenieria.usac.edu.gt>
2026-02-19 14:21:49 -0600

    primer commit

---
+++ src/main/java/com/mycompany/ipc1_ejercicios_202404225/Practica1/PuebaCommit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.ipc1_ejercicios_202404225.Practica1;
+
+/**
+ *
+ * @author ACER
+ */
+public class PuebaCommit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int  entero  ;   // solo valores ennteros 
+        entero = 2000000000 ;
+        System.out.println("entero = " + entero);
+    }
+    
+}
