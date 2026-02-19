66925d94cf940cc36a1120e35b9821cb6eb10d40
Rodrigo Gonzalez <3303393621202@ingenieria.usac.edu.gt>
2026-02-19 11:29:12 -0600

    primer commit

---
+++ src/main/java/com/mycompany/practica1/Practica1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1;
+
+/**
+ *
+ * @author ACER
+ */
+public class Practica1 {
+
+    public static void main(String[] args) {
+        double twice ;   // permite decimales tannto con o sin casting 
+        twice = 32000.55f ;   // agrega decimales aleatorios
+        System.out.println("twice = " + twice);
+    }
+}
