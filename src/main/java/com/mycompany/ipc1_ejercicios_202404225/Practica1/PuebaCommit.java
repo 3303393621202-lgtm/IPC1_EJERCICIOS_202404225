f86409f18d90eda24b2eb1d869ed61f723e6f2cc
Rodrigo Gonzalez <3303393621202@ingenieria.usac.edu.gt>
2026-02-20 22:13:46 -0600

    primer commit

--- src/main/java/com/mycompany/ipc1_ejercicios_202404225/Practica1/PuebaCommit.java
+++ src/main/java/com/mycompany/ipc1_ejercicios_202404225/Practica1/PuebaCommit.java
@@ -1,23 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package com.mycompany.ipc1_ejercicios_202404225.Practica1;
 
-/**
- *
- * @author ACER
- */
-public class PuebaCommit {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+
+
+public class PuebaCommit {
+     //variables globales 
+    private static final int FILAS_PEQUEÑO = 5 ; 
+    private static final int COLUMNAS_PEQUEÑO = 6;
+    private static final String fantasma = "@";
+    private static final String premio_simple = "0";
+    private static final String premio_especial = "$";
+    private static final String pared = "X";
+    private static final String pacman = "<";
+
+    private static Scanner sc = new Scanner(System.in);
+                    
     public static void main(String[] args) {
-        // TODO code application logic here
-        int  entero  ;   // solo valores ennteros 
-        entero = 2000000000 ;
-        System.out.println("entero = " + entero);
+        
+        System.out.println("Bienvenido a Pac-Man");
+        int menuInicio ; 
+        System.out.println("Opcioes:");
+        System.out.println("1. Crear tablero.");
+        System.out.println("2. Ver puntajes. ");
+        System.out.println("Salir");
+
+      
+               
+        
     }
     
 }
